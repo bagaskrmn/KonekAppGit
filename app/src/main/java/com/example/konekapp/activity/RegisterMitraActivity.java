036b57a41f7c38f4a9e06280dff905f35f453767
KORO-ASUS\Asus <bagaskaramnoer@gmail.com>
2023-02-24 20:00:54 +0700

    Add register to mitra activity (Error, not fixed yet)

---
+++ app/src/main/java/com/example/konekapp/activity/RegisterMitraActivity.java
@@ -0,0 +1,14 @@
+package com.example.konekapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterMitraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_mitra);
+    }
+}
