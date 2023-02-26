854c1f9be5cf030747536ce0b6692e30a13895d6
KORO-ASUS\Asus <bagaskaramnoer@gmail.com>
2023-02-26 19:51:22 +0700

    add splash screen

--- app/src/main/java/com/example/konekapp/activity/SplashScreenActivity.java
+++ app/src/main/java/com/example/konekapp/activity/SplashScreenActivity.java
@@ -2,7 +2,11 @@ package com.example.konekapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.konekapp.R;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent splashIntent = new Intent(SplashScreenActivity.this, HomeScreenActivity.class);
+                startActivity(splashIntent);
+                finish();
+            }
+        },2000);
     }
 }
