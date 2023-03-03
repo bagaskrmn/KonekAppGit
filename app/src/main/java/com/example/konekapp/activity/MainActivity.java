6725a0c07183e319e324e93a1e86b3de06ef3087
KORO-ASUS\Asus <bagaskaramnoer@gmail.com>
2023-03-03 21:52:17 +0700

    Add BottomNav on MainActivity

--- app/src/main/java/com/example/konekapp/activity/MainActivity.java
+++ app/src/main/java/com/example/konekapp/activity/MainActivity.java
@@ -5,11 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.konekapp.R;
+import com.example.konekapp.fragment.AccountFragment;
+import com.example.konekapp.fragment.ForumFragment;
+import com.example.konekapp.fragment.HomeFragment;
+import com.example.konekapp.fragment.NotificationFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,73 +27,49 @@ import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button BtnLogout, BtnMainToProfile, BtnRegisterToMitra;
-    private FirebaseAuth firebaseAuth;
-    private String currentUserId;
-    private DatabaseReference rootRef, usersRef;
-    private FirebaseUser currentUser;
+    BottomNavigationView BottomNav;
+
+    HomeFragment homeFragment = new HomeFragment();
+    ForumFragment forumFragment = new ForumFragment();
+    NotificationFragment notificationFragment = new NotificationFragment();
+    AccountFragment accountFragment = new AccountFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BtnLogout = findViewById(R.id.btnLogout);
-        BtnMainToProfile = findViewById(R.id.btnMainToProfile);
-        BtnRegisterToMitra = findViewById(R.id.btnRegisterToMitra);
+        BottomNav = findViewById(R.id.bottomNav);
 
-        firebaseAuth = FirebaseAuth.getInstance();
-        currentUser = firebaseAuth.getCurrentUser();
-        currentUserId = currentUser.getUid();
-        rootRef = FirebaseDatabase.getInstance().getReference();
-        usersRef = rootRef.child("Users");
+        //replace FrameLayout in main_activity.xml (id : container) with fragment_home.xml
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
 
-//        detect if user is mitra or not
-        usersRef.child(currentUserId).addValueEventListener(new ValueEventListener() {
+        BottomNav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                String role = snapshot.child("Role").getValue().toString();
-
-                //if user has registered as mitra
-                if (role.equals("2")) {
-                    //hide registerMitraButton
-                    BtnRegisterToMitra.setVisibility(View.GONE);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    //get id from the nav_menu
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                        //get id from the nav_menu
+                    case R.id.forum:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, forumFragment).commit();
+                        return true;
+                        //get id from the nav_menu
+                    case R.id.notification:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, notificationFragment).commit();
+                        return true;
+                        //get id from the nav_menu
+                    case R.id.account:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, accountFragment).commit();
+                        return true;
                 }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
+                return false;
             }
         });
 
-        BtnRegisterToMitra.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent RegToMitra = new Intent(MainActivity.this, RegisterMitraActivity.class);
-                startActivity(RegToMitra);
-            }
-        });
 
-        BtnLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                firebaseAuth.signOut();
-                Intent signOutIntent = new Intent(MainActivity.this, HomeScreenActivity.class);
-                signOutIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(signOutIntent);
-
-                Toast.makeText(MainActivity.this, "Logout berhasil", Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        BtnMainToProfile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent MainToProfileIntent = new Intent(MainActivity.this, ProfileActivity.class);
-                startActivity(MainToProfileIntent);
-                finish();
-            }
-        });
     }
 }
